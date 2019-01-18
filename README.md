7c20efdb6c9331849650b46a9f3016f96a3c4ebe
michalbogdal <michal.bogdal@gmil.com>
2019-01-18 01:59:52 +0100

    random numbers generated using oraclize

--- README.md
+++ README.md
@@ -1,18 +1,57 @@
 # ethereum-contract
 
-Simple lottery ethereum contract - participants can buy a coupon for 0.1 ether, each participants can buy couple of coupons (to increase the chance to win).
-The winner is determined and get almost whole included balance - 10% of it (- gas price) is send to the owner.
+Simple lottery ethereum smart contract using oraclize for generating random numbers
+- participants can bid with 0.1 ether, 
+- each participants can bid couple of times (to increase the chance to win).
+The winner take the price (90% of all bids), 10% of it (- gas price) is for organizer.
 
+
+## some commands
+
+start blockchain
+```
 truffle develop
+```
+
+run ethereum-bridge (for oraclize api)
+```
+node bridge -a 9 -H 127.0.0.1 -p 9545 --dev
+```
+
+### truffle console commands
+
+preparing accounts
+```
+let accounts; web3.eth.getAccounts(function(err,res) { accounts = res; });
+```
+
+balance
+```
+web3.eth.getBalance(accounts[0])
+```
 
 
---hints:
-let accounts;
-web3.eth.getAccounts(function(err,res) { accounts = res; });
+```
+let lottery; Lottery.deployed().then(instance => lottery=instance);
+```
 
-let lottery;
-Lottery.deployed().then(instance => lottery=instance);
+```
+lottery.bid({from: accounts[3], value: 100000000000000000})
+lottery.determineWinner({from: accounts[0], gas: 4712388})
+```
 
-lottery.buyCoupon({from: accounts[3], value: 100000000000000000}
-lottery.determineWinner({from: accounts[0], gas: 4712388}
 
+## links
+```
+https://medium.com/coinmonks/how-to-create-a-dapp-using-truffle-oraclize-ethereum-bridge-and-webpack-9cb84b8f6bcb
+```
+
+## sources or random data
+```
+oraclize_query("URL", "https://www.uuidgenerator.net/api/version4");
+oraclize_query("URL", "json(https://api.random.org/json-rpc/1/invoke).result.random.data.0", '\n{"jsonrpc":"2.0","method":"generateIntegers","params":{"apiKey":"9844ab4d-52ef-4587-9cf3-20a6930a7f6e","n":1,"min":1,"max":10,"replacement":true,"base":10},"id":1}');
+```
+
+```
+uint(keccak256(abi.encodePacked(block.number, now, participants)));
+```
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,46 +1,68 @@
 pragma solidity ^0.5.0;
 
+import "./oraclizeAPI.sol";
 
-contract Lottery {
+contract Lottery is usingOraclize {
 
-    enum LotteryState { Accepting, Finished }
+    enum LotteryState { Accepting, Finished, Paid }
 
-    address payable public owner;
+    event LogWinnerDetermined(address winner, uint256 amount);
+    event LogBidMade(address participant);
+    event LogGeneratedRandomNumber(uint randomNumber);
+
+    address payable public organizer;
     address payable[] participants;
     LotteryState state; 
-    
+
 
     constructor() public {
-        owner = msg.sender;
+        organizer = msg.sender;
         state = LotteryState.Accepting;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "only the owner can execute it"); 
+        require(msg.sender == organizer, "only the organizer can execute it"); 
         _;
     }
 
-    function buyCoupon() public payable {
+    function bid() public payable {
         require(state == LotteryState.Accepting, "lottery is already over");
-        require(msg.value == 0.1 ether, "you can spend exactly 0.1 ether for a coupon");
+        require(msg.value == 0.1 ether, "you can bid with exactly 0.1 ether");
         participants.push(msg.sender);
+
+        emit LogBidMade(msg.sender);
     }
 
-    function determineWinner() public onlyOwner {
-        state = LotteryState.Finished;
+    function determineWinner() public payable onlyOwner {
         require(participants.length > 0, "required at least one participant");
+        require(state != LotteryState.Paid, "Lottery has already been paid");
+        require(address(this).balance > oraclize_getPrice("URL"), "too low balance to pay for random number");
+
+        state = LotteryState.Finished;
+        oraclize_setProof(proofType_Ledger); // sets the Ledger authenticity proof in the constructor        
+        oraclize_query("URL", "https://www.uuidgenerator.net/api/version4");
+    }    
+
+    function __callback(bytes32 _myid, string memory _result, bytes memory _proof) public {
+        require(msg.sender == oraclize_cbAddress());
+        require(state != LotteryState.Paid, "Lottery has already been paid");
+       // if (oraclize_randomDS_proofVerify__returnCode(queryId, result, proof) != 0) {
+            //revert();
+      //  } 
+        uint maxRange = 2**(8* 7);
+        uint randomNumber = uint(keccak256(abi.encodePacked(_result))) % maxRange;
+        emit LogGeneratedRandomNumber(randomNumber);
 
         uint256 balance = address(this).balance;
         uint256 winningPrice = balance - (balance / 10);
     
-        uint256 winningIndex = random() % participants.length;
+        uint256 winningIndex = randomNumber % participants.length + 1;
         address payable winner = participants[winningIndex];
         
         winner.transfer(winningPrice);
-        selfdestruct(owner);
-    }
+        state = LotteryState.Paid;
 
-    function random() private view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty,now,participants)));
+        emit LogWinnerDetermined(winner, winningPrice);
+        selfdestruct(organizer);
     }
 }
